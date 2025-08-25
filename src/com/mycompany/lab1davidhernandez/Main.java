aef47ca4e1211c106f9dd2c67e6bf09ab610ceda
David Hernandez <david.hernandez.011011@gmail.com>
2025-08-25 12:40:26 -0400

    start and finish project, edit ZipCode.java file, write 2 constructors, GetBarCode, and ParseBarCode

--- src/com/mycompany/lab1davidhernandez/Main.java
+++ src/com/mycompany/lab1davidhernandez/Main.java
@@ -1,15 +1,83 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package com.mycompany.lab1davidhernandez;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+/**
+ *
+ * @author Nagat Drawel
+ * Lab_01: Review OOP
+ * 25/08/2025
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Required Output for Zipcode Testing");
+
+        ZipCode zip1 = new ZipCode(995047),
+                zip2 = new ZipCode("100101010011100001100110001"),
+                zip3 = new ZipCode(12345),
+                zip4 = new ZipCode(67890);
+
+
+        System.out.printf("%5d 's bar code is %s", zip1.Zip , zip1.GetBarCode());
+        System.out.printf("%n %5d 's bar code is %s", zip2.Zip, zip2.GetBarCode());
+        System.out.printf("%n %5d 's bar code is %s", zip3.Zip ,zip3.GetBarCode());
+        System.out.printf("%n %5d 's bar code is %s%n", zip4.Zip, zip4.GetBarCode());
+
+        // Test a range of values by first constructing a zip code with
+        // an integer, then retrieving the bar code and using that to
+        // construct another ZipCode.
+        int zip_int = 0;
+        for (int i = 0; i < 25; i++)
+        {
+            // Make an aribrary 5-digit zip code integer, and use it
+            // to construct a ZipCode
+            int five_digit_zip = (zip_int * zip_int) % 100000;
+            ZipCode z1 = new ZipCode(five_digit_zip);
+
+            // Construct a next ZipCode from the current one's bar code
+            String z1_code = z1.GetBarCode();
+            ZipCode z2 = new ZipCode(z1_code);
+            System.out.printf("  %02d  %05d has code %s " ,  i + 1 , z2.Zip,z1_code);
+            if ((z1_code.equals(z2.GetBarCode())) &&
+                    (z1.Zip == z2.Zip) &&
+                    (z2.Zip == five_digit_zip))
+            {
+                System.out.println(" [OK]");
+            }
+            else
+            {
+                System.out.println(z2.Zip +" "+five_digit_zip +" [ERR]");
+            }
+
+            // Increment the test value arbitrarily
+            zip_int += (233 + zip_int % 7);
+        }  // end for
+
+        System.out.println();
+        // Test some error conditions. This test assumes that
+        // ZipCode will simply print an error message to Console
+        //  and will not exit the program.
+        System.out.println("\nInitialize an array of bar codes with known errors:");
+        int BAD_ZIP_COUNT = 5;
+        // Use 2-D array of strings for tests.
+        String[ ][] badZips = {
+                { "10010101001110000110011001", "bad length" },
+                { "000101010011100001100110001", "bad start/end character" },
+                { "100101010011100001100110000", "bad start/end character" },
+                { "100104010011100001100110021","bad digit" },
+                { "100101010011100001100111001","bad sequence" }
+        };
+        for (int i = 0; i < BAD_ZIP_COUNT; i++)
+        {
+
+            System.out.printf ("Testing %s for %s %n" , badZips[i][0], badZips[i][1]);
+            // Construct ZipCode object and it will detect errors
+            ZipCode testZip = new ZipCode(badZips[i][ 0]);
+            System.out.println();
         }
-    }
+
+    }  //************** END OF main() ************    }
+
 }
--- src/com/mycompany/lab1davidhernandez/ZipCode.java
+++ src/com/mycompany/lab1davidhernandez/ZipCode.java
@@ -1,2 +1,115 @@
+package com.mycompany.lab1davidhernandez;
+
+import java.lang.Math;
+
+/**
+ * @author David Hernandez 6309110
+ * Lab 1
+ * 25/08/2025
+ */
+
 public class ZipCode {
+    protected int Zip;
+
+    public ZipCode(int num) {
+        Zip = num;
+        if (Zip > 99_999) {
+            System.out.println("Error: bar code must be 5 digits "
+                    + "long or less");
+        }
+        Zip = Zip % 100_000;
+    }
+
+    public ZipCode(String bar) {
+        if (bar.length() != 27) {
+            System.out.println("Error: bar code must be "
+                    + "in multiples of 5-binary digits");
+            return;
+        }
+
+        if (bar.charAt(0) != '1' || bar.charAt(26) != '1') {
+            System.out.println("bar code missing a 1 at start or end");
+            return;
+        }
+
+        boolean valid = true;
+        for (int i = 0; i < 27; i++) {
+            if (bar.charAt(i) != '0' && bar.charAt(i) != '1') {
+                System.out.println("bar code character: " + bar.charAt(i)
+                        + " must be '0' or '1'");
+                valid = false;
+            }
+        }
+        if (!valid) {
+            return;
+        }
+
+        int counter;
+        for (int i = 1; i <= 21; i += 5) {
+            counter = 0;
+            for (int j = i; j < i + 5; j++) {
+                if (bar.charAt(j) == '1') {
+                    counter++;
+                }
+            }
+            if (counter != 2) {
+                System.out.println(bar.substring(i, i + 5) + " has "
+                        + "invalid sequence in the bar code");
+            }
+        }
+
+        Zip = ParseBarCode(bar);
+    }
+
+    /**
+     * calculates zip code as a string of binary digits
+     * @return binary String
+     */
+    public String GetBarCode() {
+        String str = "1";
+        String zipString = String.valueOf(Zip);
+        while (zipString.length() < 5) {
+            zipString = " " + zipString;
+        }
+        for (int i = 0; i < zipString.length(); i++) {
+            str += switch(zipString.charAt(i)) {
+                case '1' -> "00011";
+                case '2' -> "00101";
+                case '3' -> "00110";
+                case '4' -> "01001";
+                case '5' -> "01010";
+                case '6' -> "01100";
+                case '7' -> "10001";
+                case '8' -> "10010";
+                case '9' -> "10100";
+                default -> "11000";
+            };
+        }
+        return str + "1";
+    }
+
+    /**
+     * decodes the binary string
+     * and returns the corresponding ZIP code as an integer
+     * @return 5 digit integer zip code
+     */
+    private static int ParseBarCode(String binary) {
+        int result = 0;
+        for (int i = 0; i < 5; i++) {
+            String num = binary.substring(5 * i + 1, 5 * i + 6);
+            result += (int) (switch(num) {
+                            case "00011" -> 1;
+                            case "00101" -> 2;
+                            case "00110" -> 3;
+                            case "01001" -> 4;
+                            case "01010" -> 5;
+                            case "01100" -> 6;
+                            case "10001" -> 7;
+                            case "10010" -> 8;
+                            case "10100" -> 9;
+                            default -> 0;
+                        } * Math.pow(10, 4 - i));
+        }
+        return result;
+    }
 }
